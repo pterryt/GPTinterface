f0b4c399d1aee326ef70698846bc402abd899e52
Philip Terry <philipterryt@gmail.com>
2023-04-12 10:08:45 -0500

    made output container wrap

--- src/gui/widgets/OutputContainer.cpp
+++ src/gui/widgets/OutputContainer.cpp
@@ -1,7 +1,9 @@
+#include <QTextEdit>
 #include "OutputContainer.h"
 
 OutputContainer::OutputContainer(QWidget *parent)
         : QListWidget(parent)
 {
-
+    setHorizontalScrollBarPolicy(Qt::ScrollBarPolicy::ScrollBarAlwaysOff);
+    setWordWrap(true);
 }
